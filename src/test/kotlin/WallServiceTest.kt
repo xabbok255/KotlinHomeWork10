21b8876277a0735487a6465660bff0c0e01bd628
xabbok255 <xabbok255@ya.ru>
2022-10-07 10:10:32 +0300

    added tests for Report class

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,9 +1,11 @@
+import comments.Comment
+import exceptions.CommentNotFoundException
 import exceptions.PostNotFoundException
+import exceptions.ReportReasonNotFoundException
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import posts.Comment
 import posts.Post
 
 class WallServiceTest {
@@ -37,20 +39,50 @@ class WallServiceTest {
     }
 
     @Test
-    fun updateNonExistingId() {
+    fun updateNonExistingPostId() {
         assertFalse(WallService.update(Post(id = 1)))
     }
 
     @Test
     fun createCommentSuccessfully() {
         WallService.add(Post())
-        val comment: Comment = Comment(text = "test")
-        assertTrue(WallService.createComment(1, comment) === comment)
+        val comment = Comment(text = "test")
+        assertTrue(WallService.createComment(1, comment) == comment)
+        assertEquals(WallService.createComment(1, Comment()).id, 2)
     }
 
     @Test(expected = PostNotFoundException::class)
-    fun createCommentException() {
+    fun createCommentWithException() {
         WallService.add(Post())
         WallService.createComment(100500, Comment())
+    }
+
+    @Test(expected = ReportReasonNotFoundException::class)
+    fun reportCommentReasonExceptionPositive() {
+        WallService.add(Post())
+        WallService.createComment(1, Comment())
+        WallService.reportComment(2, 1, 100500)
+    }
+
+    @Test(expected = ReportReasonNotFoundException::class)
+    fun reportCommentReasonExceptionNegative() {
+        WallService.add(Post())
+        WallService.createComment(1, Comment())
+        WallService.reportComment(2, 1, -100500)
+    }
+
+    @Test(expected = CommentNotFoundException::class)
+    fun reportCommentTestNoCommentException() {
+        WallService.add(Post())
+        WallService.createComment(1, Comment())
+        WallService.reportComment(2, 100500, 1)
+    }
+
+    @Test
+    fun reportCommentTestReasonSuccess() {
+        WallService.add(Post())
+        WallService.createComment(1, Comment())
+        WallService.createComment(1, Comment())
+        assertEquals(WallService.reportComment(1, 2, 5).reason, 5)
     }
 }
